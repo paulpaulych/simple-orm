64664de9cdf1f932871855f3497d34585f70534e
paulpaulych <p.pyankov@g.nsu.ru>
2020-02-13 14:14:35 +0700

    core v.1 implemented

---
+++ simple-orm-test/src/main/kotlin/simpleorm/test/ExampleEntity.kt
@@ -0,0 +1,10 @@
+package simpleorm.test
+
+data class ExampleEntity (
+    val longValue: Long,
+    val stringValue: String
+)
+
+fun main() {
+
+}
---
+++ simple-orm-test/src/test/kotlin/SchemaLoaderTest.kt
@@ -0,0 +1,12 @@
+
+import io.kotlintest.specs.FunSpec
+import simpleorm.core.schema.SchemaDeserializer
+
+class SchemaLoaderTest: FunSpec( {
+
+    test("loadSchema"){
+
+//          SchemaDeserializer().load()
+    }
+
+})
